1b5e9b52c98d0ff30fc879651af112981d73a918
proyectoMF <proyectofindegrado248@gmail.com>
2024-04-17 20:43:05 +0200

    ADD loggin view

--- app/src/main/java/com/example/athletexperience/AuthActivity.kt
+++ app/src/main/java/com/example/athletexperience/AuthActivity.kt
@@ -1,20 +1,17 @@
 package com.example.athletexperience
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.athletexperience.R.id
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_auth) // Aquí se establece el layout XML
         }
     }
-}
