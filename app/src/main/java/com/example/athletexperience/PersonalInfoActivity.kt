4ace191d2994cc08611ea1dc938500168d186a1b
ProyectoMF <proyectofindegrado248@gmail.com>
2024-05-06 21:11:01 +0200

    UPDT info_personal.xml V3

--- app/src/main/java/com/example/athletexperience/PersonalInfoActivity.kt
+++ app/src/main/java/com/example/athletexperience/PersonalInfoActivity.kt
@@ -2,13 +2,19 @@ package com.example.athletexperience
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.NumberPicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.chip.ChipGroup
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import java.util.Calendar
 
 class PersonalInfoActivity : AppCompatActivity() {
@@ -16,78 +22,122 @@ class PersonalInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_info)
 
-        // Set up the ChipGroup for gender
-        val chipGroup = findViewById<ChipGroup>(R.id.chip_group)
-        chipGroup.setOnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                R.id.chip_hombre -> {
-                    // Handle the user selecting "Hombre"
-                }
-                R.id.chip_mujer -> {
-                    // Handle the user selecting "Mujer"
-                }
-            }
+
+        val editTextFechaNacimiento = findViewById<EditText>(R.id.editTextDateFechaNacimiento)
+        val numberPickerDia = findViewById<NumberPicker>(R.id.numberPickerDia)
+        val numberPickerMes = findViewById<NumberPicker>(R.id.numberPickerMes)
+        val numberPickerAnio = findViewById<NumberPicker>(R.id.numberPickerAnio)
+
+        // Configurar NumberPicker para el día, el mes y el año
+        // Configurar NumberPicker para el día
+        numberPickerDia.minValue = 1
+        numberPickerDia.maxValue = 31
+
+        // Configurar NumberPicker para el mes
+        numberPickerMes.minValue = 1
+        numberPickerMes.maxValue = 12
+        val meses = arrayOf("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+        numberPickerMes.displayedValues = meses
+
+        // Configurar NumberPicker para el año
+        val anioActual = 2024 // Puedes obtener el año actual de forma dinámica si lo necesitas
+        numberPickerAnio.minValue = 1900
+        numberPickerAnio.maxValue = anioActual
+
+        editTextFechaNacimiento.setOnClickListener {
+            // Mostrar el layout con los NumberPicker
+            findViewById<LinearLayout>(R.id.numberPickerLayout).visibility = View.VISIBLE
         }
 
-        // Set up the EditText for date of birth
-        val dateEditText = findViewById<EditText>(R.id.editTextDateFechaNacimiento)
-        dateEditText.setOnClickListener {
-            val calendar = Calendar.getInstance()
-            val year = calendar.get(Calendar.YEAR)
-            val month = calendar.get(Calendar.MONTH)
-            val day = calendar.get(Calendar.DAY_OF_MONTH)
+        findViewById<Button>(R.id.buttonAceptarFechaNacimiento).setOnClickListener {
+            // Manejar el clic en el botón de aceptar para la fecha de nacimiento
+            val diaSeleccionado = numberPickerDia.value
+            val mesSeleccionado = numberPickerMes.value
+            val anioSeleccionado = numberPickerAnio.value
 
-            val datePickerDialog = DatePickerDialog(
-                this,
-                { _, selectedYear, selectedMonth, selectedDay ->
-                    val formattedDate = String.format("%02d-%02d-%d", selectedDay, selectedMonth + 1, selectedYear)
-                    dateEditText.setText(formattedDate)
-                },
-                year,
-                month,
-                day
-            )
-            datePickerDialog.show()
+            // Construir la fecha seleccionada en el formato deseado
+            val fechaSeleccionada = "$diaSeleccionado/$mesSeleccionado/$anioSeleccionado"
+
+            // Mostrar la fecha seleccionada en el EditText
+            editTextFechaNacimiento.setText(fechaSeleccionada)
+
+            // Ocultar el layout con los NumberPicker
+            findViewById<LinearLayout>(R.id.numberPickerLayout).visibility = View.GONE
         }
 
-        // Set up the NumberPickers for height and weight
-        val heightPicker = findViewById<NumberPicker>(R.id.heightPicker)
-        val weightPicker = findViewById<NumberPicker>(R.id.weightPicker)
-        val heightEditText = findViewById<EditText>(R.id.heightEditText)
-        val weightEditText = findViewById<EditText>(R.id.weightEditText)
-
-        // Set the text color of the NumberPickers to white
-        val textPaintComponents = heightPicker.dividerPaint.textSize
-        val textPaint = Paint()
-        textPaint.color = Color.WHITE
-        textPaint.textSize = textPaintComponents
-        heightPicker.dividerPaint = textPaint
-        weightPicker.dividerPaint = textPaint
-
-        // Set the format for the Weight NumberPicker to include a decimal point
-        weightPicker.format = "%.1f"
-
-        // Set up the labels for the NumberPickers
-        val heightLabel = findViewById<TextView>(R.id.heightLabel)
-        val weightLabel = findViewById<TextView>(R.id.weightLabel)
-
-        heightLabel.setOnClickListener {
-            heightPicker.visibility = View.VISIBLE
-            weightPicker.visibility = View.GONE
+        findViewById<Button>(R.id.buttonCancelarFechaNacimiento).setOnClickListener {
+            // Manejar el clic en el botón de cancelar para la fecha de nacimiento
+            findViewById<LinearLayout>(R.id.numberPickerLayout).visibility = View.GONE
         }
 
-        weightLabel.setOnClickListener {
-            heightPicker.visibility = View.GONE
-            weightPicker.visibility = View.VISIBLE
+
+
+
+        // Configuración de los EditText y NumberPickers para la altura y el peso
+
+        val editTextAltura = findViewById<EditText>(R.id.editTextNumberSignedAltura)
+        val editTextPeso = findViewById<EditText>(R.id.editTextNumberDecimalPeso)
+
+
+        editTextAltura.setOnClickListener {
+            // Mostrar el layout con los NumberPicker para la altura
+            findViewById<LinearLayout>(R.id.numberPickerAlturaLayout).visibility = View.VISIBLE
+            hideKeyboard()
         }
 
-        // Set up the ValueChangeListeners for the NumberPickers
-        heightPicker.setOnValueChangedListener { picker, oldVal, newVal ->
-            heightEditText.setText(newVal.toString())
+        val numberPickerAltura = findViewById<NumberPicker>(R.id.numberPickerAltura)
+        numberPickerAltura.minValue = 60
+        numberPickerAltura.maxValue = 260
+
+        findViewById<Button>(R.id.buttonAceptarAltura).setOnClickListener {
+            // Manejar el clic en el botón de aceptar para la altura
+            val alturaSeleccionada = numberPickerAltura.value
+
+            // Mostrar la altura seleccionada en el EditText
+            editTextAltura.setText(alturaSeleccionada.toString() + " cm")
+
+            // Ocultar el layout con los NumberPicker para la altura
+            findViewById<LinearLayout>(R.id.numberPickerAlturaLayout).visibility = View.GONE
         }
 
-        weightPicker.setOnValueChangedListener { picker, oldVal, newVal ->
-            weightEditText.setText(newVal.toString())
+        findViewById<Button>(R.id.buttonCancelarAltura).setOnClickListener {
+            // Manejar el clic en el botón de cancelar para la altura
+            findViewById<LinearLayout>(R.id.numberPickerAlturaLayout).visibility = View.GONE
+        }
+
+        editTextPeso.setOnClickListener {
+            // Mostrar el layout con los NumberPicker para el peso
+            findViewById<LinearLayout>(R.id.numberPickerPesoLayout).visibility = View.VISIBLE
+            hideKeyboard()
+        }
+
+
+        val numberPickerPeso = findViewById<NumberPicker>(R.id.numberPickerPeso)
+        numberPickerPeso.minValue = 30
+        numberPickerPeso.maxValue = 200
+        numberPickerPeso.setFormatter { "%.1f".format(it.toFloat()) }
+
+        findViewById<Button>(R.id.buttonAceptarPeso).setOnClickListener {
+            // Manejar el clic en el botón de aceptar para el peso
+            val pesoSeleccionado = numberPickerPeso.value
+
+            // Mostrar el peso seleccionado en el EditText
+            editTextPeso.setText(pesoSeleccionado.toString() + " Kg")
+
+            // Ocultar el layout con los NumberPicker para el peso
+            findViewById<LinearLayout>(R.id.numberPickerPesoLayout).visibility = View.GONE
+        }
+
+        findViewById<Button>(R.id.buttonCancelarPeso).setOnClickListener {
+            // Manejar el clic en el botón de cancelar para el peso
+            findViewById<LinearLayout>(R.id.numberPickerPesoLayout).visibility = View.GONE
+        }
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        currentFocus?.let {
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
         }
     }
 }
--- app/src/main/java/com/example/athletexperience/SingInActivity.kt
+++ app/src/main/java/com/example/athletexperience/SingInActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.athletexperience.databinding.ActivitySingInBinding
 import com.example.athletexperience.databinding.ActivitySingUpBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
