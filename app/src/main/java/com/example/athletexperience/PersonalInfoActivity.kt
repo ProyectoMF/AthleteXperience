aa9202ef16a8d6e26b19738a1a18d2ad9f1cbcc0
ProyectoMF <proyectofindegrado248@gmail.com>
2024-05-08 15:19:52 +0200

    UPD activity_personal_info.xml V3

--- app/src/main/java/com/example/athletexperience/PersonalInfoActivity.kt
+++ app/src/main/java/com/example/athletexperience/PersonalInfoActivity.kt
@@ -1,8 +1,10 @@
 package com.example.athletexperience
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -18,14 +20,25 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.athletexperience.loggin.SingInActivity
 import com.google.android.material.chip.ChipGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.internal.ViewUtils.hideKeyboard
 import java.util.Calendar
 
 class PersonalInfoActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_info)
 
+        //Variable para el boton de volver
+        val bt_back_personal_info = findViewById<FloatingActionButton>(R.id.bt_back_personal_info)
+
+        // Agregar OnClickListener al boton de bolver
+        bt_back_personal_info.setOnClickListener {
+            // Iniciar la anterior actividad
+            val intent = Intent(this, PersonalObjetivoActivity::class.java)
+            startActivity(intent)
+        }
 
         // Obtener las referencias de las cards de hombre y mujer
         val cardHombre = findViewById<CardView>(R.id.bt_hombre)
@@ -49,23 +62,35 @@ class PersonalInfoActivity : AppCompatActivity() {
             cardHombre.setCardBackgroundColor(ContextCompat.getColor(this, R.color.orangeDark))
         }
 
-
-        val ly_fechaNacimiento = findViewById<LinearLayout>(R.id.ly_fechaNacimiento)
+        // Configuración de los EditText y NumberPickers para la fecha
         val et_fechaNacimiento = findViewById<EditText>(R.id.et_fechaNacimiento)
         val numberPickerDia = findViewById<NumberPicker>(R.id.numberPickerDia)
         val numberPickerMes = findViewById<NumberPicker>(R.id.numberPickerMes)
         val numberPickerAnio = findViewById<NumberPicker>(R.id.numberPickerAnio)
 
         // Configurar NumberPicker para el día, el mes y el año
+
         // Configurar NumberPicker para el día
         numberPickerDia.minValue = 1
         numberPickerDia.maxValue = 31
 
-
         // Configurar NumberPicker para el mes
         numberPickerMes.minValue = 1
         numberPickerMes.maxValue = 12
-        val meses = arrayOf("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+        val meses = arrayOf(
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        )
         numberPickerMes.displayedValues = meses
 
         // Obtener el año actual de forma dinámica
@@ -78,15 +103,11 @@ class PersonalInfoActivity : AppCompatActivity() {
         // Establecer el valor por defecto del año en el 2000
         numberPickerAnio.value = 2000
 
-        ly_fechaNacimiento.setOnClickListener {
-            // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
-            hideAllNumberPickers()
-            // Mostrar el layout con los NumberPicker
-            findViewById<LinearLayout>(R.id.LayoutNumberPickerFecha).visibility = View.VISIBLE
-        }
 
         //Ocultar el teclado para el EditText
         et_fechaNacimiento.isFocusable = false
+
+        //Evento para que muestre el NumberPicker
         et_fechaNacimiento.setOnClickListener {
             // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
             hideAllNumberPickers()
@@ -116,39 +137,27 @@ class PersonalInfoActivity : AppCompatActivity() {
         }
 
 
-
-
-        // Configuración de los EditText y NumberPickers para la altura y el peso
-
-        val ly_altura = findViewById<LinearLayout>(R.id.ly_altura)
+        // Configuración de los EditText y NumberPickers para la altura
         val et_altura = findViewById<EditText>(R.id.et_altura)
-        val ly_peso = findViewById<LinearLayout>(R.id.ly_peso)
-        val et_peso = findViewById<EditText>(R.id.et_peso)
+        val numberPickerAltura = findViewById<NumberPicker>(R.id.numberPickerAltura)
 
+        //Maximo y minimo de altura
+        numberPickerAltura.minValue = 60
+        numberPickerAltura.maxValue = 260
 
-        ly_altura.setOnClickListener {
-            // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
-            hideAllNumberPickers()
-            // Mostrar el layout con los NumberPicker para la altura
-            findViewById<LinearLayout>(R.id.LayoutNumberPickerAltura).visibility = View.VISIBLE
-        }
+        // Establecer el valor por defecto de la altura en 170
+        numberPickerAltura.value = 170
 
         //Ocultar el teclado para el EditText
         et_altura.isFocusable = false
+
+        //Evento para que muestre el NumberPicker
         et_altura.setOnClickListener {
             // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
             hideAllNumberPickers()
             // Mostrar el layout con los NumberPicker
             findViewById<LinearLayout>(R.id.LayoutNumberPickerAltura).visibility = View.VISIBLE
         }
-
-
-        val numberPickerAltura = findViewById<NumberPicker>(R.id.numberPickerAltura)
-        numberPickerAltura.minValue = 60
-        numberPickerAltura.maxValue = 260
-
-        // Establecer el valor por defecto de la altura en 170
-        numberPickerAltura.value = 170
 
         findViewById<Button>(R.id.buttonAceptarAltura).setOnClickListener {
             // Manejar el clic en el botón de aceptar para la altura
@@ -166,37 +175,32 @@ class PersonalInfoActivity : AppCompatActivity() {
             findViewById<LinearLayout>(R.id.LayoutNumberPickerAltura).visibility = View.GONE
         }
 
-
+        //Configuración de los EditText y NumberPickers para el peso
+        val et_peso = findViewById<EditText>(R.id.et_peso)
         val numberPickerPeso = findViewById<NumberPicker>(R.id.numberPickerPeso)
-        // Configurar el rango del NumberPicker de peso
+        val numberPickerPesoDecimal = findViewById<NumberPicker>(R.id.numberPickerPesoDecimal)
+
+        // Configurar el mínimo y máximo de NumberPickers del peso
         numberPickerPeso.minValue = 30
         numberPickerPeso.maxValue = 200
 
         // Establecer el valor por defecto del peso en 70
         numberPickerPeso.value = 70
 
-
-        val numberPickerPesoDecimal = findViewById<NumberPicker>(R.id.numberPickerPesoDecimal)
-        // Configurar el rango del NumberPicker de peso
+        // Configurar el rango del NumberPicker del peso decimal
         numberPickerPesoDecimal.minValue = 0
         numberPickerPesoDecimal.maxValue = 9
 
-        ly_peso.setOnClickListener {
-            // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
-            hideAllNumberPickers()
-            // Mostrar el layout con los NumberPicker para el peso
-            findViewById<LinearLayout>(R.id.LayoutNumberPickerPeso).visibility = View.VISIBLE
-        }
-
         //Ocultar el teclado para el EditText
         et_peso.isFocusable = false
+
+        //Evento para mostrar el NumberPicker del peso
         et_peso.setOnClickListener {
             // Ocultar todos los NumberPicker antes de mostrar el de fecha de nacimiento
             hideAllNumberPickers()
             // Mostrar el layout con los NumberPicker
             findViewById<LinearLayout>(R.id.LayoutNumberPickerPeso).visibility = View.VISIBLE
         }
-
 
         findViewById<Button>(R.id.buttonAceptarPeso).setOnClickListener {
             // Manejar el clic en el botón de aceptar para el peso
@@ -206,7 +210,7 @@ class PersonalInfoActivity : AppCompatActivity() {
             val pesoTotalSeleccionado = "$pesoSeleccionado.$pesoDecimalSeleccionado"
 
             // Mostrar el peso seleccionado en el EditText
-            et_peso.setText(pesoTotalSeleccionado + " Kg")
+            et_peso.setText("$pesoTotalSeleccionado Kg")
 
             // Ocultar el layout con los NumberPicker para el peso
             findViewById<LinearLayout>(R.id.LayoutNumberPickerPeso).visibility = View.GONE
--- app/src/main/java/com/example/athletexperience/PersonalObjetivoActivity.kt
+++ app/src/main/java/com/example/athletexperience/PersonalObjetivoActivity.kt
@@ -1,12 +1,72 @@
 package com.example.athletexperience
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.example.athletexperience.loggin.SingInActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PersonalObjetivoActivity : AppCompatActivity(){
+
+    private lateinit var bt_back_objetive : FloatingActionButton
+    private lateinit var cv_perdergrasa : CardView
+    private lateinit var cv_mantenerpeso : CardView
+    private lateinit var cv_ganarmusculo : CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_objetivo)
+
+        initComponent()
+        initListeners()
+        initUI()
+
+    }
+
+    private fun initComponent(){
+        //Vaariable para volver atras
+        bt_back_objetive = findViewById(R.id.bt_back_objetive)
+
+        //Variables para los CardView
+        cv_perdergrasa = findViewById(R.id.cv_perdergrasa)
+        cv_mantenerpeso = findViewById(R.id.cv_mantenerpeso)
+        cv_ganarmusculo = findViewById(R.id.cv_ganarmusculo)
+    }
+
+    private fun initListeners(){
+
+        // Agregar OnClickListener a cv_perdergrasa
+        bt_back_objetive.setOnClickListener {
+            // Iniciar la anterior actividad
+            val intent = Intent(this, SingInActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Agregar OnClickListener a cv_perdergrasa
+        cv_perdergrasa.setOnClickListener {startActivity(intent)
+            // Iniciar la siguiente actividad
+            val intent = Intent(this, PersonalInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Agregar OnClickListener a cv_mantenerpeso
+        cv_mantenerpeso.setOnClickListener {
+            // Iniciar la siguiente actividad
+            val intent = Intent(this, PersonalInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Agregar OnClickListener a cv_ganarmusculo
+        cv_ganarmusculo.setOnClickListener {
+            // Iniciar la siguiente actividad
+            val intent = Intent(this, PersonalInfoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initUI() {
+
     }
 }
